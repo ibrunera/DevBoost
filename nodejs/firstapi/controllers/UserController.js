b0cc7041e8eafba6e18f4d3bc9eef330e23a0a9b
ibrunera <bteixeira010@gmail.com>
2020-08-14 16:31:18 -0300

    ...

--- nodejs/firstapi/controllers/UserController.js
+++ nodejs/firstapi/controllers/UserController.js
@@ -2,6 +2,7 @@ const users = require('../mocks/users');
 
 module.exports = {
   listUsers(request, response) {
+    console.log(request.query);
     response.writeHead(200, { 'Content-Type': 'application/json' });
     response.end(JSON.stringify(users));
   },
